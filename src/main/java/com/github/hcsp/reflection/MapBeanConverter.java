5d8e4cc8dece4ad8b22b4b63f1438e36a7c18f55
Yang Shen <55505591+doubeans@users.noreply.github.com>
2019-10-22 20:18:57 +0800

    debug (#20)
    debug (#20)

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,7 +1,10 @@
 package com.github.hcsp.reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class MapBeanConverter {
     // 传入一个遵守Java Bean约定的对象，读取它的所有属性，存储成为一个Map
@@ -12,7 +15,19 @@ public class MapBeanConverter {
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
     public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+        Map<String, Object> map = new HashMap<>();
+        Method[] methods = bean.getClass().getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.getName().startsWith("get") || method.getName().startsWith("is")) {
+                try {
+                    String key = method.getName().substring(method.getName().startsWith("get") ? 3 : 2);
+                    map.put(key.substring(0, 1).toLowerCase() + key.substring(1), method.invoke(bean));
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return map;
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -23,7 +38,18 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+        try {
+            T t = klass.getDeclaredConstructor().newInstance();
+            Set<Map.Entry<String, Object>> entries = map.entrySet();
+            for (Map.Entry<String, Object> entry : entries) {
+                String key = entry.getKey().substring(0, 1).toUpperCase() + entry.getKey().substring(1);
+                klass.getDeclaredMethod("set" + key, entry.getValue().getClass())
+                        .invoke(t, entry.getValue());
+            }
+            return t;
+        } catch (InstantiationException | InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void main(String[] args) {
