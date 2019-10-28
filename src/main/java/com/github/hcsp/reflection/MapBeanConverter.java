a89bc52ed52288f37585a0ace07887b4602325ad
JinLingfeng <37367546+JinLingfeng@users.noreply.github.com>
2019-10-28 15:25:27 +0800

    Reflection (#22)
    Reflection (#22)

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,7 +1,10 @@
 package com.github.hcsp.reflection;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+
+
 
 public class MapBeanConverter {
     // 传入一个遵守Java Bean约定的对象，读取它的所有属性，存储成为一个Map
@@ -11,8 +14,35 @@ public class MapBeanConverter {
     //  1. 读取传入参数bean的Class
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
+
+    static String produceMethodName(String str) {
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+
+    static String produceFieldName(String str) {
+        return str.substring(0, 1).toLowerCase() + str.substring(1);
+    }
+
     public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+        Map<String, Object> resultMap = new HashMap<>(10);
+        Class beanClass = bean.getClass();
+        Arrays.stream(beanClass.getDeclaredFields()).forEach(field -> {
+            try {
+                resultMap.put(field.getName(), beanClass.getDeclaredMethod("get" + produceMethodName(field.getName())).invoke(bean));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        Arrays.stream(beanClass.getDeclaredMethods())
+                .filter(x -> x.getName().startsWith("is"))
+                .forEach(x -> {
+                    try {
+                        resultMap.put(produceFieldName(x.getName().substring(2)), x.invoke(bean));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+        return resultMap;
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -23,7 +53,21 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+        T t = null;
+        try {
+            t = klass.getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        final T t1 = t;
+        map.forEach((key, val) -> {
+            try {
+                klass.getDeclaredMethod("set" + produceMethodName(key), val.getClass()).invoke(t1, val);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        return t;
     }
 
     public static void main(String[] args) {
