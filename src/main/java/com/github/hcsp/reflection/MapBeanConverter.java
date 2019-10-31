4539dddb371116e5da5db8a47cffa35ee4c483c8
dyq9410 <dyq94310@foxmail.com>
2019-11-01 00:05:53 +0800

    use an ugly way to do ....

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.reflection;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,7 +15,23 @@ public class MapBeanConverter {
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
     public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+        HashMap<String, Object> map = new HashMap<>();
+        Method[] declaredMethods = bean.getClass().getDeclaredMethods();
+
+        for (Method method : declaredMethods) {
+            try {
+                if ("void".equals(method.getGenericReturnType().getTypeName())) {
+                    continue;
+                }
+                String replace = method.getName()
+                        .replace("get", "")
+                        .replace("is", "");
+                map.put(Character.toLowerCase(replace.charAt(0)) + replace.substring(1), method.invoke(bean, null));
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+        return map;
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -23,7 +42,29 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+        T t = null;
+        try {
+            t = (T) Class.forName(klass.getName()).newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        T finalT = t;
+        map.forEach((x, y) -> {
+            try {
+                Field field = finalT.getClass().getDeclaredField(x);
+
+                field.setAccessible(true);
+                field.set(finalT, y);
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        });
+        return t;
     }
 
     public static void main(String[] args) {
