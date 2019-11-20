47f95d87538b59802931f02777d0da5d1a0ff547
Kisu <1315684099@qq.com>
2019-11-20 11:26:18 +0800

    map-bean-converter

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.reflection;
 
+import net.sf.cglib.beans.BeanMap;
+
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,7 +15,20 @@ public class MapBeanConverter {
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
     public static Map<String, Object> beanToMap(Object bean) {
+        Map<String, Object> result = new HashMap<>();
+        try {
+            result.put("id", getBeanMethod(bean, "getId"));
+            result.put("name", getBeanMethod(bean, "getName"));
+            result.put("longName", Boolean.parseBoolean(getBeanMethod(bean, "isLongName").toString()));
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
+    }
+
+    private static Object getBeanMethod(Object bean, String getName) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        return bean.getClass().getDeclaredMethod(getName, null).invoke(bean);
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -23,6 +39,15 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
+        BeanMap beanMap;
+        try {
+            T bean = klass.newInstance();
+            beanMap = BeanMap.create(bean);
+            beanMap.putAll(map);
+            return bean;
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
