646bd9b5a6e8a01a045fd4f756086dda5accfdd8
Hana <384073877@qq.com>
2019-12-05 17:58:05 +0800

    通过 Fastjson 完成

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,9 +1,13 @@
 package com.github.hcsp.reflection;
 
+import com.alibaba.fastjson.JSON;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class MapBeanConverter {
+
+
     // 传入一个遵守Java Bean约定的对象，读取它的所有属性，存储成为一个Map
     // 例如，对于一个DemoJavaBean对象 { id = 1, name = "ABC" }
     // 应当返回一个Map { id -> 1, name -> "ABC", longName -> false }
@@ -11,8 +15,10 @@ public class MapBeanConverter {
     //  1. 读取传入参数bean的Class
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
+
+
     public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+        return (Map<String, Object>) JSON.toJSON(bean);
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -22,8 +28,9 @@ public class MapBeanConverter {
     //  1. 遍历map中的所有键值对，寻找klass中名为setXXX，且参数为对应值类型的方法（即setter方法）
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
-    public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+    public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {//创建一个需要转换类型的对象
+        return JSON.parseObject(JSON.toJSONString(map), klass);
+
     }
 
     public static void main(String[] args) {
