b763f9310ad365d6e06ad40d1fa1c77002bc1dd0
恶魔妹妹买面膜 <51756846+chelegen@users.noreply.github.com>
2020-01-09 20:41:05 +0800

    反射练习 (#42)
    反射练习 (#42)

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,7 +14,21 @@ public class MapBeanConverter {
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
     public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+        Map<String, Object> map = new HashMap<>();
+        Method[] methods = bean.getClass().getMethods();
+        for (Method method : methods) {
+            String methodName = method.getName();
+            if ((methodName.contains("get") || methodName.contains("is")) && method.getParameterCount() == 0) {
+                try {
+                    String fieldName = methodName.contains("get") ? methodName.substring(3) : methodName.substring(2);
+                    fieldName = Character.toLowerCase(fieldName.charAt(0)) + fieldName.substring(1);
+                    map.put(fieldName, method.invoke(bean));
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return map;
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -23,7 +39,29 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+        T t = null;
+        try {
+            t = klass.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        Method[] methods = klass.getMethods();
+        for (Method method : methods) {
+            String methodName = method.getName();
+            if (methodName.contains("set")) {
+                String fieldName = methodName.substring(3);
+                fieldName = Character.toLowerCase(fieldName.charAt(0)) + fieldName.substring(1);
+
+                if (map.keySet().contains(fieldName)) {
+                    try {
+                        method.invoke(t, map.get(fieldName));
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return t;
     }
 
     public static void main(String[] args) {
