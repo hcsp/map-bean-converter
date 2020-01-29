c54878c0e3f9e5d35426038e1b909237a5bcfe54
monalisali <monalisali@hotmail.com>
2020-01-29 22:05:39 +0800

    done

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,7 +1,10 @@
 package com.github.hcsp.reflection;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
 
 public class MapBeanConverter {
     // 传入一个遵守Java Bean约定的对象，读取它的所有属性，存储成为一个Map
@@ -12,7 +15,25 @@ public class MapBeanConverter {
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
     public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+        List<String> methodNames = new ArrayList<>();
+        HashMap<String, Object> result = new HashMap<>();
+        Method[] methods = bean.getClass().getMethods();
+        for (Method m : methods) {
+            if (m.getName().startsWith("get") || m.getName().startsWith("is")) {
+                methodNames.add(m.getName());
+            }
+        }
+
+        for (String m : methodNames) {
+            try {
+                String key = m.startsWith("get") ? m.substring(3).toLowerCase() : StringUtils.uncapitalize(m.substring(2));
+                result.put(key, bean.getClass().getMethod(m).invoke(bean));
+            } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                throw new RuntimeException();
+            }
+        }
+
+        return result;
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -23,7 +44,31 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+        List<String> methodNames = new ArrayList<>();
+        T obj = null;
+        try {
+            obj = klass.newInstance();
+            Method[] kMethods = klass.getMethods();
+            for (Method m : kMethods) {
+                if (m.getName().startsWith("set")) {
+                    methodNames.add(m.getName());
+                }
+            }
+
+            for (String m : methodNames) {
+                String field = m.substring(3);
+                String fieldName = field.toLowerCase();
+                if (map.keySet().stream().anyMatch(x -> x.toLowerCase().equals(fieldName))) {
+                    obj.getClass().getMethod(m, map.get(fieldName).getClass()).invoke(obj, map.get(fieldName));
+                }
+            }
+
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+
+        return obj;
     }
 
     public static void main(String[] args) {
