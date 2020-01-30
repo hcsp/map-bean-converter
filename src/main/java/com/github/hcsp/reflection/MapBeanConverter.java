d5744744339f9c95452c83801b771929503e9692
NervousOrange <zch693922442@outlook.com>
2020-01-30 22:23:05 +0100

    修复了转换器只能转换固定几种成员变量的问题, 改为自动获取判断。

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,8 +1,10 @@
 package com.github.hcsp.reflection;
 
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class MapBeanConverter {
     // 传入一个遵守Java Bean约定的对象，读取它的所有属性，存储成为一个Map
@@ -13,25 +15,29 @@ public class MapBeanConverter {
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
     public static Map<String, Object> beanToMap(Object bean) {
-        Object id = getFieldOfBean(bean, "getId");
-        Object name = getFieldOfBean(bean, "getName");
-        Object isLongName = getFieldOfBean(bean, "isLongName");
         Map<String, Object> fieldsOfBean = new HashMap<>();
-        fieldsOfBean.put("id", id);
-        fieldsOfBean.put("name", name);
-        fieldsOfBean.put("longName", isLongName);
+        Stream.of(bean.getClass().getDeclaredMethods())
+                .filter(MapBeanConverter::isGetterMethod)
+                .forEach(method -> getterFieldsToMap(bean, fieldsOfBean, method));
         return fieldsOfBean;
     }
 
-    private static Object getFieldOfBean(Object bean, String method) {
-        Object field = null;
+    private static void getterFieldsToMap(Object bean, Map<String, Object> fieldsOfBean, Method method) {
         try {
-            field = bean.getClass().getMethod(method).invoke(bean);
-        } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
-            e.printStackTrace();
+            if (method.getName().startsWith("get")) {
+                fieldsOfBean.put(method.getName().substring(3, 4).toLowerCase() + method.getName().substring(4), method.invoke(bean));
+            } else {
+                fieldsOfBean.put(method.getName().substring(2, 3).toLowerCase() + method.getName().substring(3), method.invoke(bean));
+            }
+        } catch (IllegalAccessException | InvocationTargetException e) {
+           throw new RuntimeException(e);
         }
-        return field;
     }
+
+    private static boolean isGetterMethod(Method method) {
+        return method.getName().startsWith("is") || method.getName().startsWith("get");
+    }
+
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
     // 传入参数DemoJavaBean.class和Map { id -> 1, name -> "ABC"}
@@ -41,17 +47,18 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        Object id = map.get("id");
-        Object name = map.get("name");
         T obj;
         try {
             obj = klass.getConstructor().newInstance();
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             throw new RuntimeException(e);
         }
-        setFieldOfBean(obj, "setId", id);
-        setFieldOfBean(obj, "setName", name);
+        map.forEach((K, V) -> setFieldOfBean(obj, setMethod(K), V));
         return obj;
+    }
+
+    private static String setMethod(String k) {
+        return "set" + k.substring(0, 1).toUpperCase() + k.substring(1);
     }
 
     private static void setFieldOfBean(Object bean, String method, Object param) {
