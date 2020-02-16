345cd7f56ca75c47de37a557f63fe7b3d7afb250
wangyujie1207 <82414199@163.com>
2020-02-16 16:39:33 +0800

    总有一些莫名其妙的错误

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -14,21 +14,24 @@ public class MapBeanConverter {
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
     public static Map<String, Object> beanToMap(Object bean) {
-        Map<String, Object> map = new HashMap<>();
-        Method[] methods = bean.getClass().getMethods();
+        Map<String, Object> results = new HashMap<>();
+        Class clazz = bean.getClass();
+        Method[] methods = clazz.getDeclaredMethods();
         for (Method method : methods) {
-            String methodName = method.getName();
-            if ((methodName.contains("get") || methodName.contains("is")) && method.getParameterCount() == 0) {
+            String name = method.getName();
+            if (name.startsWith("is") || name.startsWith("get")) {
+                Object value = null;
                 try {
-                    String fieldName = methodName.contains("get") ? methodName.substring(3) : methodName.substring(2);
-                    fieldName = Character.toLowerCase(fieldName.charAt(0)) + fieldName.substring(1);
-                    map.put(fieldName, method.invoke(bean));
+                    value = method.invoke(bean);
                 } catch (IllegalAccessException | InvocationTargetException e) {
                     e.printStackTrace();
                 }
+                String valueName = name.split("(?<=is|get)")[1];
+                String formatedValueName = Character.toLowerCase(valueName.charAt(0)) + valueName.substring(1);
+                results.put(formatedValueName, value);
             }
         }
-        return map;
+        return results;
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -39,32 +42,25 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        T t = null;
+        T klassInstance = null;
         try {
-            t = klass.newInstance();
-        } catch (InstantiationException | IllegalAccessException e) {
+            klassInstance = klass.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             e.printStackTrace();
         }
-        Method[] methods = klass.getMethods();
-        for (Method method : methods) {
-            String methodName = method.getName();
-            if (methodName.contains("set")) {
-                String fieldName = methodName.substring(3);
-                fieldName = Character.toLowerCase(fieldName.charAt(0)) + fieldName.substring(1);
-
-                if (map.keySet().contains(fieldName)) {
-                    try {
-                        method.invoke(t, map.get(fieldName));
-                    } catch (IllegalAccessException | InvocationTargetException e) {
-                        e.printStackTrace();
-                    }
-                }
+        for (Map.Entry entry : map.entrySet()) {
+            String fieldName = (String) entry.getKey();
+            String titleCase = Character.toUpperCase(fieldName.charAt(0)) + fieldName.substring(1);
+            try {
+                klass.getMethod("set" + titleCase, map.get(fieldName).getClass()).invoke(klassInstance, entry.getValue());
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                System.out.println("Method Not Found: set " + titleCase);
             }
         }
-        return t;
+        return (T) klassInstance;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
         DemoJavaBean bean = new DemoJavaBean();
         bean.setId(100);
         bean.setName("AAAAAAAAAAAAAAAAAAA");
@@ -76,14 +72,11 @@ public class MapBeanConverter {
         System.out.println(mapToBean(DemoJavaBean.class, map));
     }
 
-    public static class DemoJavaBean {
-        private Integer id;
-        private String name;
-        private String privateField = "privateField";
+    static class DemoJavaBean {
+        Integer id;
+        String name;
 
-        public int isolate() {
-            System.out.println(privateField);
-            return 0;
+        DemoJavaBean() {
         }
 
         public Integer getId() {
@@ -96,10 +89,6 @@ public class MapBeanConverter {
 
         public String getName() {
             return name;
-        }
-
-        public String getName(int i) {
-            return name + i;
         }
 
         public void setName(String name) {
