ea7f88be855bdaba958ba9fe5e471b46902c147c
richard1230 <huyongjinnuaa@163.com>
2020-02-21 18:09:04 +0800

    感觉反射这里挺难的...自己参考了网上一个例子

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,7 +1,13 @@
 package com.github.hcsp.reflection;
 
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class MapBeanConverter {
     // 传入一个遵守Java Bean约定的对象，读取它的所有属性，存储成为一个Map
@@ -12,8 +18,23 @@ public class MapBeanConverter {
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
     public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+        Map<String, Object> map = new HashMap<>();
+        try {
+            BeanInfo beanInfo = Introspector.getBeanInfo(DemoJavaBean.class, Object.class);
+            //获取所有的属性描述器
+            PropertyDescriptor[] pds = beanInfo.getPropertyDescriptors();
+            for (PropertyDescriptor pd : pds) {
+                String key = pd.getName();
+                Method getter = pd.getReadMethod();
+                Object value = getter.invoke(bean);
+                map.put(key, value);
+            }
+            return map;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
+
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
     // 传入参数DemoJavaBean.class和Map { id -> 1, name -> "ABC"}
@@ -23,7 +44,18 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+        try {
+            T t = klass.getDeclaredConstructor().newInstance();
+            Set<Map.Entry<String, Object>> entries = map.entrySet();
+            for (Map.Entry<String, Object> entry : entries) {
+                String key = entry.getKey().substring(0, 1).toUpperCase() + entry.getKey().substring(1);
+                klass.getDeclaredMethod("set" + key, entry.getValue().getClass())
+                        .invoke(t, entry.getValue());
+            }
+            return t;
+        } catch (InstantiationException | InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void main(String[] args) {
