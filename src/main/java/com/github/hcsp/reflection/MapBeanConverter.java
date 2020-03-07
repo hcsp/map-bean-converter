43baef3829a271ded8719fa74a6ba73c72e6ec89
Felix <fiymi666@gmail.com>
2020-03-07 15:07:31 +0800

    反射练习

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.reflection;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,7 +15,34 @@ public class MapBeanConverter {
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
     public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+        Map<String, Object> map = new HashMap<>();
+        String fieldName;
+        String key = null;
+        try {
+            for (Method method : bean.getClass().getDeclaredMethods()) {
+                if (method.getName().toLowerCase().contains("get")
+                        && method.getParameterCount() == 0) {
+                    map.put(method.getName().substring(3).toLowerCase(),
+                            method.invoke(bean));
+                } else if (method.getName().toLowerCase().contains("is")
+                        && method.getParameterCount() == 0) {
+                    key = lowerCase(key, method);
+                    map.put(key, method.invoke(bean));
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+        return map;
+    }
+
+    private static String lowerCase(String key, Method method) {
+        char[] array = method.getName().substring(2).toCharArray();
+        if (array[0] >= 'A' && array[0] <= 'Z') {
+            array[0] += 32;
+            key = String.valueOf(array);
+        }
+        return key;
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -23,10 +53,23 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+        try {
+            T newInstance = klass.getConstructor().newInstance();
+            for (String attribute : map.keySet()) {
+                for (Method method : klass.getMethods()) {
+                    if (method.getName().contains("set") &&
+                            method.getName().toLowerCase().contains(attribute)) {
+                        method.invoke(newInstance, map.get(attribute));
+                    }
+                }
+            }
+            return newInstance;
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException, InstantiationException {
         DemoJavaBean bean = new DemoJavaBean();
         bean.setId(100);
         bean.setName("AAAAAAAAAAAAAAAAAAA");
