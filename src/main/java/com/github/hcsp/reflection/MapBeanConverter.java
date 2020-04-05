d672dab930ed90cb5d20588cc88e9c318c9a04d3
linkai.huang <>
2020-04-05 20:17:22 +0800

    使用反射实现一个Java Bean到Map的转换器

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,5 +1,11 @@
 package com.github.hcsp.reflection;
 
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,7 +18,19 @@ public class MapBeanConverter {
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
     public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+        Map<String, Object> map = new HashMap<>();
+        try {
+            BeanInfo info = Introspector.getBeanInfo(bean.getClass());
+            for (PropertyDescriptor pd : info.getPropertyDescriptors()) {
+                Method method = pd.getReadMethod();
+                if (method != null) {
+                    map.put(pd.getName(), method.invoke(bean));
+                }
+            }
+        } catch (IntrospectionException | InvocationTargetException | IllegalAccessException e) {
+            throw new RuntimeException();
+        }
+        return map;
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -23,7 +41,20 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+        T instance = null;
+        try {
+            instance = klass.getDeclaredConstructor().newInstance();
+            BeanInfo beanInfo = Introspector.getBeanInfo(klass);
+            for (PropertyDescriptor propertyDescriptor : beanInfo.getPropertyDescriptors()) {
+                String key = propertyDescriptor.getName();
+                if (map.containsKey(key)) {
+                    propertyDescriptor.getWriteMethod().invoke(instance, map.get(key));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return instance;
     }
 
     public static void main(String[] args) {
