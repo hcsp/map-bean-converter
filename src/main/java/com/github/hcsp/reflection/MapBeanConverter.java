eeedcc60877d5434ea30d659490b9e9e8eaa178c
YongJieLiu <61576871+Truthcode@users.noreply.github.com>
2020-04-19 10:05:06 +0800

    Liu (#83)
    Liu (#83)

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,7 +1,12 @@
 package com.github.hcsp.reflection;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
 
 public class MapBeanConverter {
     // 传入一个遵守Java Bean约定的对象，读取它的所有属性，存储成为一个Map
@@ -12,7 +17,28 @@ public class MapBeanConverter {
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
     public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+        Map<String, Object> map = new HashMap<>();
+        try {
+            getMethodContent(bean, map);
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        return map;
+    }
+
+    private static Map getMethodContent(Object bean, Map map) throws InvocationTargetException, IllegalAccessException {
+        Method[] declaredMethods = bean.getClass().getDeclaredMethods();
+        for (Method method : declaredMethods) {
+            if (method.getName().startsWith("get") || method.getName().startsWith("is")) {
+                if (method.getParameterCount() == 0) {
+                    String valueName = method.getName().split("(?<=is|get)")[1];
+                    String formatedValueName = Character.toLowerCase(valueName.charAt(0)) + valueName.substring(1);
+
+                    map.put(formatedValueName, method.invoke(bean));
+                }
+            }
+        }
+        return map;
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -23,7 +49,22 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+        try {
+            T klassInstance = klass.getDeclaredConstructor().newInstance();
+            BeanInfo beanInfo = Introspector.getBeanInfo(klassInstance.getClass());
+            PropertyDescriptor[] pds = beanInfo.getPropertyDescriptors();
+
+            for (PropertyDescriptor pd : pds) {
+                Object value = map.get(pd.getName());
+                Method setter = pd.getWriteMethod();
+                if (value != null) {
+                    setter.invoke(klassInstance, value);
+                }
+            }
+            return klassInstance;
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | IntrospectionException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void main(String[] args) {
@@ -43,9 +84,9 @@ public class MapBeanConverter {
         private String name;
         private String privateField = "privateField";
 
-        public int isolate() {
+        public String isolate() {
             System.out.println(privateField);
-            return 0;
+            return null;
         }
 
         public Integer getId() {
