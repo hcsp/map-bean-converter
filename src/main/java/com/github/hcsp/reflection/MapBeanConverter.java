e8a3464638d6ef6598b2288327e953de82bd7b4d
gofocus <go_focus@live.cn>
2020-05-11 15:55:41 +0800

    map-bean-converter

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,7 +1,11 @@
 package com.github.hcsp.reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MapBeanConverter {
     // 传入一个遵守Java Bean约定的对象，读取它的所有属性，存储成为一个Map
@@ -11,8 +15,37 @@ public class MapBeanConverter {
     //  1. 读取传入参数bean的Class
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
-    public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+    public static Map<String, Object> beanToMap(Object bean) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        return Stream.of(bean.getClass().getDeclaredMethods())
+                .filter(method -> method.getParameterCount() == 0)
+                .filter(MapBeanConverter::isGetter)
+                .collect(Collectors.toMap(MapBeanConverter::getFieldName, method -> invokeMethod(bean, method)));
+    }
+
+    private static boolean isGetter(Method method) {
+        String methodName = method.getName();
+        return (methodName.startsWith("get") && methodName.length() > 3) ||
+                (methodName.startsWith("is") && methodName.length() > 2 && method.getReturnType() == boolean.class);
+    }
+
+    private static String getFieldName(Method method) {
+        if (!isGetter(method)) {
+            return "";
+        }
+        String methodName = method.getName();
+        String fieldName = methodName.split("is|get")[1];
+        char[] chars = fieldName.toCharArray();
+        chars[0] = Character.toLowerCase(chars[0]);
+        return String.valueOf(chars);
+    }
+
+    private static Object invokeMethod(Object bean, Method method) {
+        try {
+            return method.invoke(bean);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -23,10 +56,38 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+        try {
+            T instance = klass.getConstructor().newInstance();
+            map.forEach((key, value) -> {
+                Method setMethod = getSetMethod(key, klass, value);
+                setValue(instance, setMethod, value);
+            });
+            return instance;
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 
-    public static void main(String[] args) {
+    private static <T> void setValue(T instance, Method method, Object value) {
+        try {
+            method.invoke(instance, value);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static <T> Method getSetMethod(String key, Class<T> tClass, Object value) {
+        try {
+            return tClass.getDeclaredMethod("set" + key.substring(0, 1).toUpperCase() + key.substring(1), value.getClass());
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         DemoJavaBean bean = new DemoJavaBean();
         bean.setId(100);
         bean.setName("AAAAAAAAAAAAAAAAAAA");
@@ -43,7 +104,7 @@ public class MapBeanConverter {
         private String name;
         private String privateField = "privateField";
 
-        public int isolate() {
+        private int isolate() {
             System.out.println(privateField);
             return 0;
         }
