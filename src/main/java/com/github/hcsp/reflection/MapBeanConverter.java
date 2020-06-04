410f6238983b7aa241562b8ef133c6612bae069b
Sunpeng526 <215860865@qq.com>
2020-06-04 02:24:47 -0700

    submit 1 time

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,32 +1,57 @@
 package com.github.hcsp.reflection;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MapBeanConverter {
-    // 传入一个遵守Java Bean约定的对象，读取它的所有属性，存储成为一个Map
-    // 例如，对于一个DemoJavaBean对象 { id = 1, name = "ABC" }
-    // 应当返回一个Map { id -> 1, name -> "ABC", longName -> false }
-    // 提示：
-    //  1. 读取传入参数bean的Class
-    //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
-    //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
-    public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+
+    /*
+     * 传入一个遵守Java Bean约定的对象，读取它的所有属性，存储成为一个Map 例如，对于一个DemoJavaBean对象 { id = 1, name
+     * = "ABC" } 应当返回一个Map { id -> 1, name -> "ABC", longName -> false } 提示： 1. *
+     * 读取传入参数bean的Class 2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法） 3.
+     * 通过反射调用这些方法并将获得的值存储到Map中返回
+     * 
+     */
+    public static Map<String, Object> beanToMap(Object bean) throws IllegalAccessException, IllegalArgumentException,
+            InvocationTargetException, NoSuchMethodException, SecurityException {
+        Map<String, Object> result = new HashMap<>();
+        Class<? extends Object> clazz = bean.getClass();
+        Method[] methods = clazz.getDeclaredMethods();
+        for (int i = 0; i < methods.length; i++) {
+            if (methods[i].getParameterCount() == 0 && methods[i].getName().startsWith("get")
+                    || methods[i].getParameterCount() == 0 && methods[i].getName().startsWith("is")
+                            && methods[i].getReturnType().equals(Boolean.class)) {
+                result.put(methods[i].getName(), methods[i].invoke(bean));
+            }
+        }
+        return result;
     }
 
-    // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
-    // 传入参数DemoJavaBean.class和Map { id -> 1, name -> "ABC"}
-    // 应当返回一个DemoJavaBean对象 { id = 1, name = "ABC" }
-    // 提示：
-    //  1. 遍历map中的所有键值对，寻找klass中名为setXXX，且参数为对应值类型的方法（即setter方法）
-    //  2. 使用反射创建klass对象的一个实例
-    //  3. 使用反射调用setter方法对该实例的字段进行设值
-    public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+    /*
+     * 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例 // 传入参数DemoJavaBean.class和Map { id
+     * -> 1, name -> "ABC"} // 应当返回一个DemoJavaBean对象 { id = 1, name = "ABC" } // 提示：
+     * // 1. 遍历map中的所有键值对，寻找klass中名为setXXX，且参数为对应值类型的方法（即setter方法） // 2.
+     * 使用反射创建klass对象的一个实例 // 3. 使用反射调用setter方法对该实例的字段进行设值
+     */
+    public static <T> T mapToBean(Class<T> klass, Map<String, Object> map)
+            throws InstantiationException, IllegalAccessException {
+        T t = klass.newInstance();
+        Field[] fileds = klass.getDeclaredFields();
+        for (int i = 0; i < fileds.length; i++) {
+            String key = fileds[i].getName();
+            if (map.containsKey(key)) {
+                fileds[i].setAccessible(true);
+                fileds[i].set(t, map.get(key));
+            }
+        }
+        return t;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException, IllegalArgumentException,
+            InvocationTargetException, NoSuchMethodException, SecurityException, InstantiationException {
         DemoJavaBean bean = new DemoJavaBean();
         bean.setId(100);
         bean.setName("AAAAAAAAAAAAAAAAAAA");
@@ -74,15 +99,7 @@ public class MapBeanConverter {
 
         @Override
         public String toString() {
-            return "DemoJavaBean{"
-                    + "id="
-                    + id
-                    + ", name='"
-                    + name
-                    + '\''
-                    + ", longName="
-                    + isLongName()
-                    + '}';
+            return "DemoJavaBean{" + "id=" + id + ", name='" + name + '\'' + ", longName=" + isLongName() + '}';
         }
     }
 }
