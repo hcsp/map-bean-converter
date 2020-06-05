2351815d227f5acac5c3f01050f36dd544928677
JinbingLin <59944908+352503538@users.noreply.github.com>
2020-06-04 19:07:27 -0700

    利用反射获取信息并赋值 (#93)
    利用反射获取信息并赋值 (#93)

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,5 +1,9 @@
 package com.github.hcsp.reflection;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,7 +16,30 @@ public class MapBeanConverter {
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
     public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+        if (bean == null) {
+            return null;
+        }
+
+        Map<String, Object> map = new HashMap<>();
+
+        Field[] declaredFields = bean.getClass().getDeclaredFields();
+        Method[] methods = bean.getClass().getDeclaredMethods();
+        try {
+            for (Field field : declaredFields) {
+                field.setAccessible(true);
+                map.put(field.getName(), field.get(bean));
+                for (Method method : methods) {
+                    if (method.getName().equals("isLongName")) {
+                        map.put("longName", method.invoke(bean));
+                    }
+                }
+            }
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+
+        return map;
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -23,14 +50,36 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+        if (map == null) {
+            return null;
+        }
+        T obj = null;
+        try {
+            obj = klass.newInstance();
+
+            Field[] fields = obj.getClass().getDeclaredFields();
+            for (Field field : fields) {
+                int mod = field.getModifiers();
+                if (Modifier.isStatic(mod) || Modifier.isFinal(mod)) {
+                    continue;
+                }
+
+                field.setAccessible(true);
+                field.set(obj, map.get(field.getName()));
+            }
+        } catch (IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        }
+        return obj;
     }
 
     public static void main(String[] args) {
         DemoJavaBean bean = new DemoJavaBean();
         bean.setId(100);
         bean.setName("AAAAAAAAAAAAAAAAAAA");
+
         System.out.println(beanToMap(bean));
+
 
         Map<String, Object> map = new HashMap<>();
         map.put("id", 123);
