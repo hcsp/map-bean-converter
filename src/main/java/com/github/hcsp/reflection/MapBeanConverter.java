771d0a002478689ece4e490224cb2de6b9e877d8
Sunpeng526 <55687152+Sunpeng526@users.noreply.github.com>
2020-06-05 16:54:02 +0800

    Sunpeng123 (#94)
    Sunpeng123 (#94)

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,32 +1,68 @@
 package com.github.hcsp.reflection;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MapBeanConverter {
-    // 传入一个遵守Java Bean约定的对象，读取它的所有属性，存储成为一个Map
-    // 例如，对于一个DemoJavaBean对象 { id = 1, name = "ABC" }
-    // 应当返回一个Map { id -> 1, name -> "ABC", longName -> false }
-    // 提示：
-    //  1. 读取传入参数bean的Class
-    //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
-    //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
-    public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+
+    public static Map<String, Object> beanToMap(Object bean) throws IllegalArgumentException, SecurityException {
+        Map<String, Object> result = new HashMap<>();
+        Class<? extends Object> clazz = bean.getClass();
+        Method[] methods = clazz.getDeclaredMethods();
+        for (int i = 0; i < methods.length; i++) {
+            if ((methods[i].getParameterCount() == 0 && methods[i].getName().startsWith("get"))) {
+                try {
+                    result.put(Character.toLowerCase(methods[i].getName().charAt(3))+ methods[i].getName().substring(4), methods[i].invoke(bean));
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            if ((methods[i].getParameterCount() == 0 && methods[i].getName().startsWith("is")
+                    && methods[i].getReturnType().equals(Boolean.TYPE))) {
+                try {
+                    result.put(Character.toLowerCase(methods[i].getName().charAt(2))+methods[i].getName().substring(3), methods[i].invoke(bean));
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
     }
 
-    // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
-    // 传入参数DemoJavaBean.class和Map { id -> 1, name -> "ABC"}
-    // 应当返回一个DemoJavaBean对象 { id = 1, name = "ABC" }
-    // 提示：
-    //  1. 遍历map中的所有键值对，寻找klass中名为setXXX，且参数为对应值类型的方法（即setter方法）
-    //  2. 使用反射创建klass对象的一个实例
-    //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+        T t = null;
+        try {
+            t = klass.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        Field[] fileds = klass.getDeclaredFields();
+        for (int i = 0; i < fileds.length; i++) {
+            String key = fileds[i].getName();
+            if (map.containsKey(key)) {
+                fileds[i].setAccessible(true);
+                try {
+                    fileds[i].set(t, map.get(key));
+                } catch (IllegalArgumentException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+        return t;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException, IllegalArgumentException,
+            InvocationTargetException, NoSuchMethodException, SecurityException, InstantiationException {
         DemoJavaBean bean = new DemoJavaBean();
         bean.setId(100);
         bean.setName("AAAAAAAAAAAAAAAAAAA");
@@ -74,15 +110,7 @@ public class MapBeanConverter {
 
         @Override
         public String toString() {
-            return "DemoJavaBean{"
-                    + "id="
-                    + id
-                    + ", name='"
-                    + name
-                    + '\''
-                    + ", longName="
-                    + isLongName()
-                    + '}';
+            return "DemoJavaBean{" + "id=" + id + ", name='" + name + '\'' + ", longName=" + isLongName() + '}';
         }
     }
 }
