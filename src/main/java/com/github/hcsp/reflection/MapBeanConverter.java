4ba07a8a0fb0fa6c9b089ca54e6bd481adcbf760
nicknessii <58057685+nicknessii@users.noreply.github.com>
2020-06-15 19:53:47 +0800

    使用反射实现一个Java Bean到Map的转换器 (#98)
    使用反射实现一个Java Bean到Map的转换器 (#98)

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,5 +1,11 @@
 package com.github.hcsp.reflection;
 
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,7 +18,24 @@ public class MapBeanConverter {
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
     public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+        Map<String, Object> map = new HashMap<>();
+        BeanInfo info = null;
+        try {
+            info = Introspector.getBeanInfo(bean.getClass());
+        } catch (IntrospectionException e) {
+            throw new RuntimeException(e);
+        }
+        for (PropertyDescriptor propertyDescriptor : info.getPropertyDescriptors()) {
+            Method method = propertyDescriptor.getReadMethod();
+            if (null != method) {
+                try {
+                    map.put(propertyDescriptor.getName(), method.invoke(bean));
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return map;
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -23,7 +46,30 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+        T instance = null;
+        try {
+            instance = klass.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+        BeanInfo beanInfo = null;
+        try {
+            beanInfo = Introspector.getBeanInfo(klass);
+        } catch (IntrospectionException e) {
+            throw new RuntimeException(e);
+        }
+        for (PropertyDescriptor propertyDescriptor : beanInfo.getPropertyDescriptors()) {
+            String key = propertyDescriptor.getName();
+            if (map.containsKey(key)) {
+                try {
+                    propertyDescriptor.getWriteMethod().invoke(instance, map.get(key));
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return instance;
+
     }
 
     public static void main(String[] args) {
@@ -86,3 +132,4 @@ public class MapBeanConverter {
         }
     }
 }
+
