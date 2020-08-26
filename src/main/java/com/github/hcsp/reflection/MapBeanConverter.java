c70042813487e66a2966d7846c623778b2d0b516
Ryan <49991585+Ryan-Y-C@users.noreply.github.com>
2020-08-26 11:18:23 +0800

    使用反射实现Java Bean到Map的转换器 (#114)
    使用反射实现Java Bean到Map的转换器 (#114)

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,7 +1,15 @@
 package com.github.hcsp.reflection;
 
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
 
 public class MapBeanConverter {
     // 传入一个遵守Java Bean约定的对象，读取它的所有属性，存储成为一个Map
@@ -12,7 +20,34 @@ public class MapBeanConverter {
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
     public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+        Map<String, Object> map = new HashMap<>();
+        Class c = bean.getClass();
+        Set<String> methodsName = new TreeSet<>();
+        Method[] methods = ReflectionUtils.getDeclaredMethods(c);
+        for (Method method : methods) {
+            methodsName.add(method.getName());
+        }
+        filedValueIntoMap(bean, map, c, methodsName);
+        return map;
+    }
+
+    private static void filedValueIntoMap(Object bean, Map<String, Object> map, Class c, Set<String> methodsName) {
+        for (Field declaredField : c.getDeclaredFields()) {
+            String beanField = "get" + captureName(declaredField.getName());
+            for (String methodName : methodsName) {
+                if (beanField.equals(methodName)) {
+                    beanIntoMap(bean, map, c, beanField, declaredField.getName());
+                }
+                if ("isLongName".equals(methodName)) {
+                    beanIntoMap(bean, map, c, methodName, "longName");
+                }
+            }
+        }
+    }
+
+    private static void beanIntoMap(Object bean, Map<String, Object> map, Class c, String beanField, String name) {
+        Method m = ReflectionUtils.findMethod(c, beanField);
+        map.put(name, ReflectionUtils.invokeMethod(m, bean));
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -23,7 +58,29 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+        T obj;
+        try {
+            obj = klass.getConstructor().newInstance();
+            Field[] fields = klass.getDeclaredFields();
+            Method[] methods = ReflectionUtils.getDeclaredMethods(klass);
+
+            for (Field f : fields) {
+                String beanField = "set" + captureName(f.getName());
+                for (Method method : methods) {
+                    if (beanField.equals(method.getName())) {
+                        for (Map.Entry<String, Object> entry : map.entrySet()) {
+                            if (f.getName().equals(entry.getKey())) {
+                                ReflectionUtils.invokeMethod(method, obj, entry.getValue());
+                            }
+                        }
+                    }
+                }
+            }
+
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException();
+        }
+        return obj;
     }
 
     public static void main(String[] args) {
@@ -36,6 +93,12 @@ public class MapBeanConverter {
         map.put("id", 123);
         map.put("name", "ABCDEFG");
         System.out.println(mapToBean(DemoJavaBean.class, map));
+    }
+
+    private static String captureName(String str) {
+        char[] cs = str.toCharArray();
+        cs[0] -= 32;
+        return String.valueOf(cs);
     }
 
     public static class DemoJavaBean {
