71fe6906ad14a795636b1ee0e6000bea6a9fe4c1
luckzhu <luckzhu1992@gmail.com>
2020-10-13 09:49:00 +0800

    map-bean-converter (#124)
    map-bean-converter (#124)

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,5 +1,9 @@
 package com.github.hcsp.reflection;
 
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.PropertyUtils;
+
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,7 +16,13 @@ public class MapBeanConverter {
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
     public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+        Map<String, Object> properties = new HashMap<>();
+        try {
+            properties = PropertyUtils.describe(bean);
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return properties;
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -23,7 +33,14 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+        T bean = null;
+        try {
+            bean = klass.newInstance();
+            BeanUtils.populate(bean, map);
+        } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            e.printStackTrace();
+        }
+        return bean;
     }
 
     public static void main(String[] args) {
@@ -79,14 +96,14 @@ public class MapBeanConverter {
         @Override
         public String toString() {
             return "DemoJavaBean{"
-                    + "id="
-                    + id
-                    + ", name='"
-                    + name
-                    + '\''
-                    + ", longName="
-                    + isLongName()
-                    + '}';
+                   + "id="
+                   + id
+                   + ", name='"
+                   + name
+                   + '\''
+                   + ", longName="
+                   + isLongName()
+                   + '}';
         }
     }
 }
