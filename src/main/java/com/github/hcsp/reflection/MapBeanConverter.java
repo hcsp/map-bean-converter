f1183442db31d2ba191d444f1b0565509f3fd5ad
catmmao <catmmao@gmail.com>
2021-01-26 20:24:18 +0800

    实现 java bean 和 map 的转换 (#132)
    实现 java bean 和 map 的转换 (#132)

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,7 +1,11 @@
 package com.github.hcsp.reflection;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class MapBeanConverter {
     // 传入一个遵守Java Bean约定的对象，读取它的所有属性，存储成为一个Map
@@ -12,7 +16,25 @@ public class MapBeanConverter {
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
     public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+        try {
+            return Arrays.stream(
+                    Introspector.getBeanInfo(bean.getClass(), Object.class)
+                            .getPropertyDescriptors()
+            )
+                    .filter(pd -> Objects.nonNull(pd.getReadMethod()))
+                    .collect(Collectors.toMap(
+                            PropertyDescriptor::getName,
+                            pd -> {
+                                try {
+                                    return pd.getReadMethod().invoke(bean);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                    return null;
+                                }
+                            }));
+        } catch (IntrospectionException e) {
+            return Collections.emptyMap();
+        }
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -23,6 +45,22 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
+        try {
+            T beanInstance = klass.getConstructor().newInstance();
+
+            for (String key : map.keySet()) {
+                Object value = map.get(key);
+                Field field = klass.getDeclaredField(key);
+                field.setAccessible(true);
+                field.set(beanInstance, value);
+                field.setAccessible(false);
+            }
+
+            return beanInstance;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
