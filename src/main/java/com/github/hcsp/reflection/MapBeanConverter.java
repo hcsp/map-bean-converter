8826c9884ddee836b98dd1764d8d0c46390bf9e1
hedongsheng <he77505357@163.com>
2022-01-24 15:57:54 +0800

    反射 (#153)
    反射 (#153)

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -3,6 +3,8 @@ package com.github.hcsp.reflection;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.alibaba.fastjson.JSON;
+
 public class MapBeanConverter {
     // 传入一个遵守Java Bean约定的对象，读取它的所有属性，存储成为一个Map
     // 例如，对于一个DemoJavaBean对象 { id = 1, name = "ABC" }
@@ -12,7 +14,8 @@ public class MapBeanConverter {
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
     public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+        Object result = JSON.toJSON(bean);
+        return (Map<String, Object>) result;
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -23,7 +26,7 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+        return JSON.parseObject(JSON.toJSONString(map), klass);
     }
 
     public static void main(String[] args) {
