bcb287d8e919e100d43c86f08d5358ffa8aa96cd
TQ <756342530@qq.com>
2022-02-16 19:54:25 +0800

    使用反射实现一个Java Bean到Map的转换器

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,7 +1,13 @@
 package com.github.hcsp.reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MapBeanConverter {
     // 传入一个遵守Java Bean约定的对象，读取它的所有属性，存储成为一个Map
@@ -12,7 +18,10 @@ public class MapBeanConverter {
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
     public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+        return Stream.of(bean.getClass().getDeclaredMethods())
+                .filter(MapBeanConverter::isGetter)
+                .collect(Collectors.toMap(MapBeanConverter::getFieldName,
+                        method -> invokeGetter(bean, method)));
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -23,7 +32,40 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+        T bean;
+        try {
+            bean = klass.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            String mapKey = entry.getKey();
+            Object mapValue = entry.getValue();
+            Method method = getSetter(klass,mapKey);
+            if (method != null) {
+                invokeSetter(bean, mapValue, method);
+            }
+        }
+        return bean;
+    }
+
+    private static <T> void invokeSetter(T bean, Object mapValue, Method method) {
+        try {
+            method.invoke(bean, mapValue);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static <T> Method getSetter(Class<T> klass,String key) {
+        Method declaredMethod = null;
+        try {
+            declaredMethod = klass.getDeclaredMethod("set" + fieldNameToUpperCase(key), klass.getDeclaredField(key).getType());
+        } catch (NoSuchMethodException | NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        return declaredMethod;
     }
 
     public static void main(String[] args) {
@@ -36,6 +78,35 @@ public class MapBeanConverter {
         map.put("id", 123);
         map.put("name", "ABCDEFG");
         System.out.println(mapToBean(DemoJavaBean.class, map));
+    }
+
+    private static boolean isGetter(Method method) {
+        String methodName = method.getName();
+
+        return (methodName.startsWith("get") && methodName.length() > 3
+                && method.getParameters().length == 0
+        )
+                || (methodName.startsWith("is") && methodName.length() > 2
+                && Character.isUpperCase(methodName.charAt(2)));
+    }
+
+    private static String getFieldName(Method method) {
+        return fieldNameToLowerCase(method.getName().split("is|get")[1]);
+    }
+
+    private static String fieldNameToLowerCase(String string) {
+        return string.replace(string.charAt(0), Character.toLowerCase(string.charAt(0)));
+    }
+    private static String fieldNameToUpperCase(String string) {
+        return string.replace(string.charAt(0), Character.toUpperCase(string.charAt(0)));
+    }
+
+    private static Object invokeGetter(Object bean, Method method) {
+        try {
+            return method.invoke(bean);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException();
+        }
     }
 
     public static class DemoJavaBean {
