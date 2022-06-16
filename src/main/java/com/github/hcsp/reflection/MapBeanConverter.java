1d07d417c669e02c948a471bb6191c7c00c3b6fa
LiuXing <94578246+iamxliu@users.noreply.github.com>
2022-06-16 11:22:02 +0800

    反射 (#156)
    反射 (#156)

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,7 +1,12 @@
 package com.github.hcsp.reflection;
 
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MapBeanConverter {
     // 传入一个遵守Java Bean约定的对象，读取它的所有属性，存储成为一个Map
@@ -12,8 +17,33 @@ public class MapBeanConverter {
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
     public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+        return Stream.of(bean.getClass()
+                        .getDeclaredMethods())
+                .filter(MapBeanConverter::isGetterMethod)
+                .collect(Collectors.toMap(MapBeanConverter::getFieldName, method -> {
+                    try {
+                        return method.invoke(bean);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    } catch (InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                }));
     }
+
+    private static String getFieldName(Method object) {
+        String name = object.getName().split("get|is")[1];
+        return Character.toLowerCase(name.charAt(0)) + name.substring(1);
+    }
+
+    private static boolean isGetterMethod(Method method) {
+        String methodName = method.getName();
+        if (method.getParameterCount() != 0) {
+            return false;
+        }
+        return (methodName.startsWith("is") && methodName.length() > 2 && Character.isUpperCase(methodName.charAt(2))) || (methodName.startsWith("get") && methodName.length() > 3 && Character.isUpperCase(methodName.charAt(3)));
+    }
+
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
     // 传入参数DemoJavaBean.class和Map { id -> 1, name -> "ABC"}
@@ -23,7 +53,38 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+
+        try {
+            T bean = klass.getConstructor().newInstance();
+            map.forEach((key, value) -> {
+                Method method = setterMethod(klass, key);
+                if (method != null) {
+                    try {
+                        method.invoke(bean, value);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    } catch (InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+            return bean;
+
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException(e);
+//            e.printStackTrace();
+        }
+
+    }
+
+    private static <T> Method setterMethod(Class<T> klass, String key) {
+        String methodName = "set" + Character.toUpperCase(key.charAt(0)) + key.substring(1);
+        try {
+            return klass.getDeclaredMethod(methodName, klass.getDeclaredField(key).getType());
+        } catch (NoSuchMethodException | NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void main(String[] args) {
@@ -78,15 +139,7 @@ public class MapBeanConverter {
 
         @Override
         public String toString() {
-            return "DemoJavaBean{"
-                    + "id="
-                    + id
-                    + ", name='"
-                    + name
-                    + '\''
-                    + ", longName="
-                    + isLongName()
-                    + '}';
+            return "DemoJavaBean{" + "id=" + id + ", name='" + name + '\'' + ", longName=" + isLongName() + '}';
         }
     }
 }
