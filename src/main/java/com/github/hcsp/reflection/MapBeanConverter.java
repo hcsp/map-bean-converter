111bb02529b85d307ab418b62ef55e29048e01b9
wjxalexander <37684072+wjxalexander@users.noreply.github.com>
2023-05-07 11:23:51 +0800

    done (#161)
    done (#161)

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,7 +1,8 @@
 package com.github.hcsp.reflection;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
 
 public class MapBeanConverter {
     // 传入一个遵守Java Bean约定的对象，读取它的所有属性，存储成为一个Map
@@ -11,8 +12,37 @@ public class MapBeanConverter {
     //  1. 读取传入参数bean的Class
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
-    public static Map<String, Object> beanToMap(Object bean) {
+    private static boolean isGetterMethod(String method, String keyword) {
+        return method.startsWith(keyword) && method.length() > 3 && Character.isUpperCase(method.charAt(keyword.length()));
+    }
+
+
+    private static boolean isGetMethodWithoutParameter(Method method) {
+        return method.getParameterCount() == 0 && isGetterMethod(method.getName(), "get") || isGetterMethod(method.getName(), "is");
+    }
+
+    private static String removeGetterPrefix(String getterName) {
+        if (isGetterMethod(getterName, "get")) {
+            return Character.toLowerCase(getterName.charAt(3)) + getterName.substring(4);
+        } else if (isGetterMethod(getterName, "is")) {
+            return Character.toLowerCase(getterName.charAt(2)) + getterName.substring(3);
+        }
+        // If the method name does not start with "get" or "is," it is not a getter and does not have a property key.
         return null;
+    }
+
+    public static Map<String, Object> beanToMap(Object bean) {
+        Map<String, Object> ret = new HashMap<>();
+        Class<?> myClass = bean.getClass();
+        Method[] methods = myClass.getDeclaredMethods();
+        Arrays.stream(methods).filter(MapBeanConverter::isGetMethodWithoutParameter).forEach(method -> {
+            try {
+                ret.put(removeGetterPrefix(method.getName()), method.invoke(bean));
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        return ret;
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -23,7 +53,23 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
-        return null;
+        T bean;
+        try {
+            bean = klass.getConstructor().newInstance();
+            map.forEach((key, value) -> {
+                String setterName = "set" + key.substring(0, 1).toUpperCase() + key.substring(1);
+                try {
+                    klass.getMethod(setterName, value.getClass()).invoke(bean, value);
+                } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+
+        return bean;
     }
 
     public static void main(String[] args) {
