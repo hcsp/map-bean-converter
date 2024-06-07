bb9625f083db7670c723a69000dca29cc6795327
Leo Cheung <leozhang621@gmail.com>
2024-06-07 17:17:24 +0800

    map bean converter (#165)
    map bean converter (#165)

--- src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
+++ src/main/java/com/github/hcsp/reflection/MapBeanConverter.java
@@ -1,7 +1,9 @@
 package com.github.hcsp.reflection;
 
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class MapBeanConverter {
     // 传入一个遵守Java Bean约定的对象，读取它的所有属性，存储成为一个Map
@@ -12,7 +14,26 @@ public class MapBeanConverter {
     //  2. 通过反射获得它包含的所有名为getXXX/isXXX，且无参数的方法（即getter方法）
     //  3. 通过反射调用这些方法并将获得的值存储到Map中返回
     public static Map<String, Object> beanToMap(Object bean) {
-        return null;
+        Class<?> cls = bean.getClass();
+        Map<String, Object> res = new HashMap<>();
+        try {
+            Method[] methods = cls.getMethods();
+            for (Method method : methods) {
+                String methodName = method.getName();
+                if (
+                        (Pattern.matches("is[A-Z].+", methodName) || Pattern.matches("get[A-Z].+", methodName))
+                                && method.getParameterCount() == 0) {
+                    String fieldName = getFieldNameFromGetterName(methodName);
+                    if (fieldName.isEmpty()) {
+                        continue;
+                    }
+                    res.put(fieldName, method.invoke(bean));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return res;
     }
 
     // 传入一个遵守Java Bean约定的Class和一个Map，生成一个该对象的实例
@@ -23,7 +44,31 @@ public class MapBeanConverter {
     //  2. 使用反射创建klass对象的一个实例
     //  3. 使用反射调用setter方法对该实例的字段进行设值
     public static <T> T mapToBean(Class<T> klass, Map<String, Object> map) {
+        try {
+            T obj = klass.getConstructor().newInstance();
+            for (Map.Entry<String, Object> kv : map.entrySet()) {
+                String setterName = buildSetterName(kv.getKey());
+                klass.getMethod(setterName, kv.getValue().getClass()).invoke(obj, kv.getValue());
+            }
+            return obj;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
+    }
+
+    public static String getFieldNameFromGetterName(String getterName) {
+        if (getterName.startsWith("is") && getterName.length() > 2) {
+            return getterName.substring(2, 3).toLowerCase() + getterName.substring(3);
+        } else if (getterName.startsWith("get") && getterName.length() > 3) {
+            return getterName.substring(3, 4).toLowerCase() + getterName.substring(4);
+        }
+        return "";
+    }
+
+    public static String buildSetterName(String fieldName) {
+        String upper = fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
+        return "set" + upper;
     }
 
     public static void main(String[] args) {
@@ -83,10 +128,10 @@ public class MapBeanConverter {
                     + id
                     + ", name='"
                     + name
-                    + '\''
+                    + "'"
                     + ", longName="
                     + isLongName()
-                    + '}';
+                    + "}";
         }
     }
 }
